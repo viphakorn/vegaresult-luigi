c45d1a5bad9a5a089cb6068277e5ea346ca3e19c
viphakorn <viphakone585@gmail.com>
2023-02-18 11:28:40 +0700

    add classes

--- script.js
+++ script.js
@@ -2,44 +2,38 @@ const menuButton = document.querySelector(".menu-button")
 const navList = document.querySelector("#navigation-list")
 
 menuButton.addEventListener("click", () => {
- const isOpened = menuButton.getAttribute("aria-expanded") === "true"
- isOpened ? closeMenu() : openMenu()
+  const isOpened = menuButton.getAttribute("aria-expanded") === "true"
+  isOpened ? closeMenu() : openMenu()
 })
 
 function openMenu() {
- menuButton.setAttribute("aria-expanded", "true")
- navList.setAttribute("data-state", "opened")
+  menuButton.setAttribute("aria-expanded", "true")
+  navList.setAttribute("data-state", "opened")
 }
 function closeMenu() {
- menuButton.setAttribute("aria-expanded", "false")
- navList.setAttribute("data-state", "closing")
+  menuButton.setAttribute("aria-expanded", "false")
+  navList.setAttribute("data-state", "closing")
 
- navList.addEventListener(
-  "animationend",
-  () => {
-   navList.setAttribute("data-state", "closed")
-  },
-  { once: true }
- )
+  navList.addEventListener("animationend", () => navList.setAttribute("data-state", "closed"), { once: true })
 }
 
 const dropdownButton = document.querySelector("#dropdown-button")
 const dropdownList = document.querySelector(".dropdown-list")
 
 dropdownButton.addEventListener("click", () => {
- dropdownList.hidden = !dropdownList.hidden
- dropdownButton.setAttribute("aria-expanded", !dropdownList.hidden)
+  const isOpened = dropdownButton.getAttribute("aria-expanded") === "true"
+  isOpened ? dropdownButton.setAttribute("aria-expanded", "false") : dropdownButton.setAttribute("aria-expanded", "true")
 })
 
 const filterSearch = document.querySelector("#filter-search")
 const tableRows = document.querySelectorAll("#players-table tbody tr")
 
 filterSearch.addEventListener("input", () => {
- const searchTerm = filterSearch.value.trim().toLowerCase()
+  const searchTerm = filterSearch.value.trim().toLowerCase()
 
- tableRows.forEach((row) => {
-  const [id, name, fed, title, rtgF, rtgN] = Array.from(row.querySelectorAll("td")).map((cell) => cell.textContent.trim().toLowerCase())
-  const matches = [id, name, fed, title, rtgF, rtgN].some((field) => field.includes(searchTerm))
-  row.style.display = matches ? "" : "none"
- })
+  tableRows.forEach((row) => {
+    const [id, name, fed, title, rtgF, rtgN] = Array.from(row.querySelectorAll("td")).map((cell) => cell.textContent.trim().toLowerCase())
+    const matches = [id, name, fed, title, rtgF, rtgN].some((field) => field.includes(searchTerm))
+    row.style.display = matches ? "" : "none"
+  })
 })
